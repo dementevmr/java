0e15954d32f41d964a3d680be7c0f3278cb09ed7
Nikita <k1takanat@gmail.com>
2022-01-09 15:57:47 +0500

    first commit

---
+++ Script.js
@@ -0,0 +1,43 @@
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt('Как ваше имя?', );
+//answers[1] = prompt('Как ваша фамилия?', );
+//answers[2] = prompt('Сколько вам лет?', );
+
+//document.write(answers); 
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+//console.log(`https://someurl.com/_{category}/5`);
+
+//const user = "Nikita";
+
+//alert(`Privet, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10;
+	decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 =='8');
+
+const isChecked = true;
+	isClose = false;
+
+	console.log(isChecked && isClose);
+
+	console.log(isChecked || isClose);
